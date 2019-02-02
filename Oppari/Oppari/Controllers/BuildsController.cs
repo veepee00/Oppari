bad1962d8d7b1dd4ec6452863449970fab8b2977
veepee00 <35451981+veepee00@users.noreply.github.com>
2019-02-02 20:06:17 +0200

    Projektin uudelleen teko .Net Core 2.2 versiolla
    Tehty käyttöliittymä kahdella napilla, joista pystyy suorittamaan tiedosto- ja tietokantahaut.

---
+++ Oppari/Oppari/Controllers/BuildsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Oppari.Models;
+
+namespace Oppari.Controllers
+{
+    public class BuildsController : Controller
+    {
+        private readonly ComputerBuildingContext _context;
+
+        public BuildsController(ComputerBuildingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Builds
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Builds.ToListAsync());
+        }
+
+        // GET: Builds/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var build = await _context.Builds
+                .FirstOrDefaultAsync(m => m.BuildId == id);
+            if (build == null)
+            {
+                return NotFound();
+            }
+
+            return View(build);
+        }
+
+        // GET: Builds/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Builds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BuildId,Title")] Build build)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(build);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(build);
+        }
+
+        // GET: Builds/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var build = await _context.Builds.FindAsync(id);
+            if (build == null)
+            {
+                return NotFound();
+            }
+            return View(build);
+        }
+
+        // POST: Builds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BuildId,Title")] Build build)
+        {
+            if (id != build.BuildId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(build);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BuildExists(build.BuildId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(build);
+        }
+
+        // GET: Builds/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var build = await _context.Builds
+                .FirstOrDefaultAsync(m => m.BuildId == id);
+            if (build == null)
+            {
+                return NotFound();
+            }
+
+            return View(build);
+        }
+
+        // POST: Builds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var build = await _context.Builds.FindAsync(id);
+            _context.Builds.Remove(build);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BuildExists(int id)
+        {
+            return _context.Builds.Any(e => e.BuildId == id);
+        }
+    }
+}
---
+++ Oppari/Oppari/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Oppari.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Oppari.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void ExecuteFileTests()
+        {
+
+        }
+
+        public void ExecuteSqlQueryTests()
+        {
+
+        }
+
+        public string CheckOldFilesFromDirectory(string folder, string mask, int time = -10)
+        {
+            if (folder == "" || folder == null || mask == "" || mask == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            List<string> files = new List<string>(Directory.GetFiles(folder));
+            List<string> returnFiles = new List<string>();
+
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                if ((fi.LastWriteTime < DateTime.Now.AddMinutes(time)) && fi.Extension == mask)
+                {
+                    Console.WriteLine(fi.Extension);
+                    returnFiles.Add(file);
+                }
+            }
+
+            if (returnFiles.Count() > 0)
+            {
+                time *= -1;
+                return "Tiedostojen lukumäärä, joihin ei ole koskettu " + time.ToString() + " minuutin sisällä: " + returnFiles.Count().ToString();
+            }
+            else
+            {
+                return "Tiedostojen lukumäärä, joihin ei ole koskettu " + time.ToString() + " minuutin sisällä ei löytynyt!";
+            }
+            
+        }
+
+        public string CheckSqlQueries()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ComputerBuildingContext>();
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ComputerBuilding_Db;Trusted_Connection=True;ConnectRetryCount=0");
+            using (var context = new ComputerBuildingContext(optionsBuilder.Options))
+            {
+                var builds = context.Builds.FromSql("SELECT * FROM dbo.Builds").ToList();
+                if (builds.Count() > 0)
+                {
+                    return "Tietokannasta löytyi " + builds.Count().ToString() + " tietokonetta.";
+                }
+                else
+                {
+                    return "Tietokannasta ei löytynyt yhtään tietokonetta!";
+                }
+            }          
+        }
+    }
+}
